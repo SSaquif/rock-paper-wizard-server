1a89710af15dfe8fbbd8b1bdd0bf26aa4d62ab70
Sadnan Saquif <ssaquif@gmail.com>
2024-02-04 03:53:09 -0500

    added in express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-import { db } from "./adapters/db/index.js";
+import express from "express";
 import "dotenv/config";
+import { db } from "./adapters/db/index.js";
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
