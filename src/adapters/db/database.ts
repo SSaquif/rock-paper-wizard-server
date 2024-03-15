62cea67095b26e77b6865c16428920b50255d58f
Sadnan Saquif <ssaquif@gmail.com>
2024-03-14 23:02:52 -0400

    adding missing roundtable to DB interface

--- src/adapters/db/database.ts
+++ src/adapters/db/database.ts
@@ -1,8 +1,10 @@
 import { ColumnType } from "kysely";
 import { GameTable } from "../../models/games.model.js";
 import { UserTable } from "../../models/users.model.js";
+import { RoundTable } from "../../models/rounds.model.js";
 export interface RockPaperWizardDatabase {
   games: GameTable;
+  rounds: RoundTable;
   archived_games: Omit<GameTable, "deleted_at"> & {
     archived_at: ColumnType<Date, string | undefined, never>;
   };
