9ea58e29917f6381f537afd3abf8af6971cc7e4f
Sadnan Saquif <ssaquif@gmail.com>
2024-03-14 22:54:10 -0400

    error handling middleware

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-import express, { application } from "express";
+import express, { ErrorRequestHandler, application } from "express";
 import "dotenv/config";
-import { db } from "./adapters/db/index.js";
-import { User } from "./models/users.model.js";
 // import { uuidv4 } from "uuid";
 import Websocket, { WebSocketServer } from "ws";
 import UserRouter from "./routes/users.route.js";
 import GameRouter from "./routes/games.route.js";
 import helmet from "helmet";
 import morgan from "morgan";
+import {
+  errorLogger,
+  clientErrorHandler,
+  errorHandler,
+} from "./middlewares/error.handler.js";
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -22,6 +25,11 @@ app.get("/", (req, res) => {
 
 app.use("/api/users", UserRouter);
 app.use("/api/games", GameRouter);
+
+// error handling middleware
+app.use(errorLogger);
+app.use(clientErrorHandler);
+app.use(errorHandler);
 
 const httpServer = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ src/middlewares/error.handler.ts
@@ -0,0 +1,28 @@
+import { XHR_ERRORS } from "@ssaquif/rock-paper-wizard-api-types-and-schema";
+import { ErrorRequestHandler } from "express";
+
+// todo: consider setting up a Monad to handle errors
+
+export const errorLogger: ErrorRequestHandler = (err, req, res, next) => {
+  // todo: consider logging the error to a file or db or slack/discord channel
+  console.error(err.message);
+  console.error(err.stack);
+  next(err);
+};
+
+export const clientErrorHandler: ErrorRequestHandler = (
+  error,
+  req,
+  res,
+  next
+) => {
+  if (req.xhr) {
+    return res.status(500).json({ error: XHR_ERRORS.GENERIC_ERROR });
+  } else {
+    next(error);
+  }
+};
+
+export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  return res.status(500).json({ message: err.message });
+};
