ecb2f26996a0d7736d71fcd018c6fe2f4ff17d6a
Sadnan Saquif <ssaquif@gmail.com>
2024-03-14 22:53:54 -0400

    updated controller and service

--- src/controllers/games.controller.ts
+++ src/controllers/games.controller.ts
@@ -9,3 +9,18 @@ export const createNewGameController: RequestHandler = (req, res, next) => {
     next(error);
   }
 };
+
+export const joinGameController: RequestHandler = (req, res, next) => {
+  try {
+    // const result = createNewGame(req);
+    // return res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const startGameController: RequestHandler = (req, res, next) => {};
+
+export const leaveGameController: RequestHandler = (req, res, next) => {};
+
+export const cancelGameController: RequestHandler = (req, res, next) => {};
--- src/services/games.service.ts
+++ src/services/games.service.ts
@@ -1,33 +1,69 @@
 import { Request } from "express";
 import { db } from "../adapters/db/index.js";
-import { NewGameEntrySchema } from "@ssaquif/rock-paper-wizard-api-types-and-schema";
-import { NewGame } from "../models/games.model.js";
+import {
+  NewGameEntrySchema,
+  XHR_ERRORS,
+  APIErrorResponse,
+} from "@ssaquif/rock-paper-wizard-api-types-and-schema";
+import { Game, NewGame } from "../models/games.model.js";
+import { IntRange } from "../types/utility.types.js";
 
-export const createNewGame = async (req: Request) => {
+export const createNewGame = async (
+  req: Request
+): Promise<Game | APIErrorResponse> => {
   const validatedFormInput = NewGameEntrySchema.safeParse(req.body);
   if (!validatedFormInput.success) {
     throw new Error(validatedFormInput.error.issues[0].message);
   }
 
-  // const { username, numOfPlayers, password, confirmPassword } =
-  //   validatedFormInput.data;
-
-  const dataToInsert = {
-    username: validatedFormInput.data.username,
-    numOfPlayers: validatedFormInput.data.numOfPlayers,
+  const dataToInsert: NewGame = {
+    player_1: validatedFormInput.data.username,
+    player_1_points: 0,
+    player_1_position: 6,
+    number_of_players: validatedFormInput.data.numOfPlayers as IntRange<2, 6>,
     password: validatedFormInput.data.password,
+    current_round: 0,
+    game_status: "creating",
   };
 
-  // - generate a new game_id
-  // - add created_at timestamp
-  // - add updated_at timestamp
-  // - add password
-  // - add player_1 name
-  // - add number_of_players
-  // - based on number of players, set non need player columns to null
-  // - set all player gold to 0
-  // const result = await db
-  //   .insertInto("games")
-  //   .values(dataToInsert)
-  //   .executeTakeFirst();
+  // todo: determine if I need to return all
+  const result = await db
+    .insertInto("games")
+    .values(dataToInsert)
+    .returningAll()
+    .executeTakeFirstOrThrow();
+
+  return result;
+};
+
+export const joinGame = async (
+  req: Request
+): Promise<Game | APIErrorResponse> => {
+  const { game_id, username, password } = req.body;
+  const game = await db
+    .selectFrom("games")
+    .selectAll()
+    .where("game_id", "=", game_id)
+    .executeTakeFirst();
+
+  if (!game) {
+    return { error: XHR_ERRORS.GAME_NOT_FOUND };
+  }
+  if (game.password !== password) {
+    return { error: XHR_ERRORS.INVALID_PASSWORD };
+  }
+
+  const updatedGame = {
+    ...game,
+    [`player_${game.number_of_players}`]: username,
+  };
+
+  const result = await db
+    .updateTable("games")
+    .set(updatedGame)
+    .where("game_id", "=", game_id)
+    .returningAll()
+    .executeTakeFirstOrThrow();
+
+  return result;
 };
