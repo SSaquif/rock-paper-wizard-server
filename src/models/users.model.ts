8f8fa43c26db8f9fed5756c009cdc006392fc58f
Sadnan Saquif <ssaquif@gmail.com>
2025-02-23 22:55:53 -0500

    User handling router setup

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -9,8 +9,15 @@ import {
 } from "kysely";
 
 export interface UserTable {
-  email: string;
+  user_id: string;
   password: string;
+  created_at: ColumnType<"timestamp">;
+  updated_at: ColumnType<"timestamp">;
+  // For later
+  // email: string;
+  // is_verified: boolean;
 }
 
 export type User = Selectable<UserTable>;
+export type NewUser = Insertable<UserTable>;
+export type UpdatedUser = Updateable<UserTable>;
