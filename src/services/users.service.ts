7a88eed9d5233c4281536ee1dc7824f203ba3510
Sadnan Saquif <ssaquif@gmail.com>
2025-06-03 14:04:45 -0400

    Auth changes

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -3,8 +3,10 @@ import { Request, Response } from "express";
 import { db } from "../adapters/db/index.js";
 import {
   AuthenticatedUser,
+  LoginFormSchema,
   SYSTEM_ERRORS,
   User,
+  UserRegistrationFormSchema,
 } from "@ssaquif/rock-paper-wizard-api-types-and-schema";
 
 export const getUsersService = async (
@@ -26,11 +28,19 @@ export const registerUserService = async (
 ): Promise<AuthenticatedUser> => {
   //@todo: Figure out way to properly type the request body
   const { username, password, confirmPassword } = req.body;
-  // @todo: Remove Duplicate Validation if the Zod validation is working
-  if (password !== confirmPassword) {
+
+  // Validate using zod schema
+  const validatedData = UserRegistrationFormSchema.safeParse({
+    username,
+    password,
+    confirmPassword,
+  });
+
+  if (!validatedData.success) {
     return {
-      error: SYSTEM_ERRORS.PASSWORD_MISMATCH,
       isError: true,
+      error: SYSTEM_ERRORS.ZOD_SCHEMA_VALIDATION_ERROR,
+      message: validatedData.error.issues[0].message,
     };
   }
 
@@ -43,14 +53,15 @@ export const registerUserService = async (
 
   if (existingUser.length) {
     return {
-      error: SYSTEM_ERRORS.EXISTING_USER,
       isError: true,
+      error: SYSTEM_ERRORS.EXISTING_USER,
     };
   }
 
-  // encrypt the password
+  // Encrypt the password
   const hashedPassword = await bycrypt.hash(password, 10);
 
+  // Insert the user into the db
   const user = await db
     .insertInto("users")
     .values({
@@ -60,18 +71,19 @@ export const registerUserService = async (
     .returning(["user_id", "created_at", "updated_at"])
     .executeTakeFirst();
 
+  // Check if user was inserted
   if (!user) {
     return {
-      error: SYSTEM_ERRORS.DB_INSERT_ERROR,
       isError: true,
+      error: SYSTEM_ERRORS.DB_INSERT_ERROR,
     };
   }
 
   return {
+    isError: false,
     user_id: user.user_id,
     created_at: user.created_at,
     updated_at: user.updated_at,
-    isError: false,
   };
 };
 
@@ -80,32 +92,44 @@ export const loginUserService = async (
 ): Promise<AuthenticatedUser> => {
   //@todo: Figure out way to properly type the request body
   const { username, password } = req.body;
+
+  // Validate using zod schema
+  const validatedData = LoginFormSchema.safeParse({ username, password });
+  if (!validatedData.success) {
+    return {
+      isError: true,
+      error: SYSTEM_ERRORS.ZOD_SCHEMA_VALIDATION_ERROR,
+      message: validatedData.error.issues[0].message,
+    };
+  }
+
   const user = await db
     .selectFrom("users")
     .where("user_id", "=", username)
     .selectAll()
     .executeTakeFirst();
 
-  // check if user exists
+  // Check if user exists
   if (!user) {
     return {
-      error: SYSTEM_ERRORS.USER_NOT_FOUND,
       isError: true,
+      error: SYSTEM_ERRORS.USER_NOT_FOUND,
     };
   }
 
+  // Check if password is correct
   const match = await bycrypt.compare(password, user.password);
   if (!match) {
     return {
-      error: SYSTEM_ERRORS.INVALID_PASSWORD,
       isError: true,
+      error: SYSTEM_ERRORS.INVALID_PASSWORD,
     };
   }
 
   return {
+    isError: false,
     user_id: user.user_id,
     created_at: user.created_at,
     updated_at: user.updated_at,
-    isError: false,
   };
 };
