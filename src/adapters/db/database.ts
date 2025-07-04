3447a7201ae1277a8efc4215b7756a60d8de357f
Sadnan Saquif <ssaquif@gmail.com>
2025-07-03 20:53:04 -0400

    updated db model

--- src/adapters/db/database.ts
+++ src/adapters/db/database.ts
@@ -2,11 +2,14 @@ import { ColumnType } from "kysely";
 import { RPWGameTable } from "../../models/rpw_games.model.js";
 import { UserTable } from "../../models/users.model.js";
 import { RoundTable } from "../../models/rpw_rounds.model.js";
+import { SessionTable } from "../../models/sessions.model.js";
 export interface BoardGamesDatabase {
+  users: UserTable;
   rpw_games: RPWGameTable;
+  sessions: SessionTable;
   rpw_rounds: RoundTable;
+  // unused at the moment
   rpw_archived_games: Omit<RPWGameTable, "deleted_at"> & {
     archived_at: ColumnType<Date, string | undefined, never>;
   };
-  users: UserTable; // just for testing for now
 }
